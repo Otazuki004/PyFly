d93176e095a4528dc44e475642921d170145ab7b
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-07 17:01:04 +0530

    Update __init__.py

--- root/__init__.py
+++ root/__init__.py
@@ -1,1 +1,7 @@
 from pyrogram import Client
+
+a_id = "10187126" # Your Api Id
+a_hash = "ff197c0d23d7fe54c89b44ed092c1752" # Your Api Hash 
+b_tok = "6957348263:AAH7yp5PMl1pIHhtxRuMbOvnK9FwvKshOuk" # Your Bot Token
+
+bot = Client("PyFly™", bot_token=b_tok, api_id=a_id, api_hash=a_hash, plugins=dict(root="root/Server"))
