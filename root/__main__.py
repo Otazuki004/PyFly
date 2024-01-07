0e9882271872b3cc054d1695e75725e43fe32e9a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-07 16:56:08 +0530

    Update __main__.py

--- root/__main__.py
+++ root/__main__.py
@@ -3,10 +3,10 @@ from pyrogram import *
 import os
 import sys
 import io
-from ACCESS import rootsys, rootuser, admin, data
+from root import bot
+from root import *
+import asyncio
 
-
-bot.start()
 print("Getting Ready")
 print("[INFO] PyFly Started")
 print("[INFO] Running Until Disconnected")
@@ -14,10 +14,16 @@ print("[INFO] Running Until Disconnected")
 @bot.on_message(filters.command("start"))
 async def PyFly(_, message):
     getting_ready = await bot.send_message(message.chat.id, "Starting PyFly..")
-    
+    await asyncio.sleep(2)
+    await getting_ready.edit("PyFly Has Stated")
+
 @bot.on_message(filters.command(['run', 'bash', 'sh', 'shell']))
 async def PyFly(_, message):
     try:
         Run_Txt = await message.reply_text("`....`")
     except Exception as e:
         await message.reply_text(f"**Error**: {e}")
+
+bot.run()
+print("[INFO] PyFly Has Stoped")
+exit()
