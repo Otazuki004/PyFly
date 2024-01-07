8d4f237ba4f5d2a10625e317440dde2e194bc7e8
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-07 21:43:04 +0530

    Delete root directory

--- root/__init__.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client
-
-a_id = "10187126" # Your Api Id
-a_hash = "ff197c0d23d7fe54c89b44ed092c1752" # Your Api Hash 
-b_tok = "6957348263:AAH7yp5PMl1pIHhtxRuMbOvnK9FwvKshOuk" # Your Bot Token
-
-SUDO = [5965055071]
-
-bot = Client("PyFly", bot_token=b_tok, api_id=a_id, api_hash=a_hash, plugins=dict(root="root/Properties"))
